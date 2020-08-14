c7d3ddd171865fd97929624d4d43c5a83faadead
KryptoJP <67850655+KryptoJP@users.noreply.github.com>
2020-08-14 03:07:07 -0400

    Create embed.js

---
+++ commands/embed.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+
+module.exports = {
+	name: 'embed',
+	description: 'Embeds Message',
+	execute(message, args) {
+    message.delete();
+    if (!args.length) return message.channel.send(`It seems you haven't given me anyting to say ${message.author}! Try typing something after the say command.`);
+    const response = args.join(' ');
+    
+    const exampleEmbed = new Discord.MessageEmbed()
+    .setColor('GOLD')
+    .setAuthor(`${message.member.user.tag}`, `${message.author.displayAvatarURL()}`)
+  	.setDescription(response)
+	  .setFooter('Embed created by Kry!')
+
+		message.channel.send(exampleEmbed);
+    
+	},
+};
